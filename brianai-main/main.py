6708301d651dd8712adebdf1d984de288c07c1d1
António Martinho <81374425+AntonioPMartinho@users.noreply.github.com>
2021-12-09 09:56:57 +0000

    Add files via upload

---
+++ brianai-main/main.py
@@ -0,0 +1,53 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'brian' in command:
+                command = command.replace('brian', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_brian():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Could you please say that again.')
+
+
+while True:
+    run_brian()
