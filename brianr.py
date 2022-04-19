05b5f33ef6cd1ec3e25e9a75297e85a3dd1e1544
António Martinho <81374425+AntonioPMartinho@users.noreply.github.com>
2022-04-20 00:38:04 +0100

    Add files via upload

---
+++ brianr.py
@@ -0,0 +1,278 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+
+from numpy import who
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import os
+import time
+import pyttsx3
+import speech_recognition as sr
+import pytz
+import requests
+import subprocess
+import wikipedia
+import random
+import webbrowser
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+MONTHS = ["january", "february", "march", "april", "may", "june","july", "august", "september","october","november", "december"]
+DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("" + str(e))
+
+    return said.lower()
+
+
+def authenticate_google():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    return service
+
+
+def get_events(day, service):
+    # Call the Calendar API
+    date = datetime.datetime.combine(day, datetime.datetime.min.time())
+    end_date = datetime.datetime.combine(day, datetime.datetime.max.time())
+    utc = pytz.UTC
+    date = date.astimezone(utc)
+    end_date = end_date.astimezone(utc)
+
+    events_result = service.events().list(calendarId='primary', timeMin=date.isoformat(), timeMax=end_date.isoformat(),
+                                        singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        speak('No upcoming events found.')
+    else:
+        speak(f"You have {len(events)} events on this day.")
+
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+            start_time = str(start.split("T")[1].split("-")[0]) 
+            if int(start_time.split(":")[0]) < 12:
+                start_time = start_time + "am"
+            else:
+                start_time = str(int(start_time.split(":")[0])-12) + start_time.split(":")[1]
+                start_time = start_time + "pm"
+                
+            speak(event["summary"] + " at " + start_time)
+
+def get_date(text):
+    text = text.lower()
+    today = datetime.date.today()
+
+    if text.count("today") > 0:
+        return today
+
+    day = -1
+    day_of_week = -1
+    month = -1
+    year = today.year
+
+    for word in text.split():
+        if word in MONTHS:
+            month = MONTHS.index(word) + 1
+        elif word in DAYS:
+            day_of_week = DAYS.index(word)
+        elif word.isdigit():
+            day = int(word)
+        else:
+            for ext in DAY_EXTENTIONS:
+                found = word.find(ext)
+                if found > 0:
+                    try:
+                        day = int(word[:found])
+                    except:
+                        pass
+
+    
+    if month < today.month and month != -1:  
+        year = year+1
+
+    if month == -1 and day != -1:  
+        if day < today.day:
+            month = today.month + 1
+        else:
+            month = today.month
+
+    # if we only found a dta of the week
+    if month == -1 and day == -1 and day_of_week != -1:
+        current_day_of_week = today.weekday()
+        dif = day_of_week - current_day_of_week
+
+        if dif < 0:
+            dif += 7
+            if text.count("next") >= 1:
+                dif += 7
+
+        return today + datetime.timedelta(dif)
+
+    if day != -1:  
+        return datetime.date(month=month, day=day, year=year)
+
+def note(text):
+    date = datetime.datetime.now()
+    file_name = str(date).replace(":", "-") + "-note.txt"
+    with open(file_name, "w") as f:
+        f.write(text)
+
+    subprocess.Popen(["notepad.exe", file_name])
+
+
+
+
+
+
+
+
+WAKE = "hey brian"
+SERVICE = authenticate_google()
+print("Start")
+
+while True:
+    print("Listening")
+    text = get_audio()
+
+    if text.count(WAKE) > 0:
+        speak("I am ready")
+        text = get_audio()
+
+        CALENDAR_STRS = ["what do i have", "do i have plans", "am i busy"]
+        for phrase in CALENDAR_STRS:
+            if phrase in text:
+                date = get_date(text) 
+                if date:
+                    get_events(date, SERVICE)
+                else:
+                    speak("I don't understand")
+
+        NOTE_STRS = ["make a note", "write this down", "remember this"]
+        for phrase in NOTE_STRS:
+            if phrase in text:
+                speak("What would you like me to write down?")
+                note_text = get_audio()
+                note(note_text)
+                speak("I've made a note of that.")
+
+
+        WEATHER_STRS = ["weather","temperature","forecast","forecaste"]
+        for phrase in WEATHER_STRS:
+            if phrase in text:
+                api_key="3b5ac6b8b0ae1bcd0c44312981297188"
+                base_url="https://api.openweathermap.org/data/2.5/weather?"
+                speak("whats the city name")
+                city_name=get_audio()
+                complete_url=base_url+"appid="+api_key+"&q="+city_name
+                response = requests.get(complete_url)
+                x=response.json()
+                if x["cod"]!="404":
+                    y=x["main"]
+                    current_temperature = y["temp"]
+                    current_temperature = current_temperature - 273.15
+                    current_temperature = "{:.0f}".format(current_temperature)
+                    current_humidiy = y["humidity"]
+                    z = x["weather"]
+                    weather_description = z[0]["description"]
+                    speak(" Temperature in Celsius is " +   
+                        str(current_temperature) + "degrees" +
+                        "\n humidity in percentage is " +
+                        str(current_humidiy) +
+                        "\n description  " +
+                        str(weather_description))
+                    print(" Temperature in Celsius = " +
+                        str(current_temperature) +
+                        "\n humidity (in percentage) = " +
+                        str(current_humidiy) +
+                        "\n description = " +
+                        str(weather_description))
+
+                else:
+                    speak(" City Not Found ")
+
+
+        WIKIPEDIA_STRS=["look up"," wikipedia", "definition"," what is"]
+        for phrase in WIKIPEDIA_STRS:
+            if phrase in text:
+                speak("What would you like to search on wikipedia?")
+                search = get_audio()
+                speak('Searching '+ search +'on Wikipedia...')
+                results = wikipedia.summary(search, sentences=2)
+                speak("According to Wikipedia")
+                print(results)
+                speak(results)
+           
+                
+        
+
+
+        COINFLIP_STRS=["toss","flip","coin","coinflip"]
+        for phrase in COINFLIP_STRS:
+            if phrase in text:
+                 moves=["head", "tails"]   
+                 cmove=random.choice(moves)
+                 speak("The computer chose " + cmove)
+                 print("The computer chose: " + cmove)
+
+        
+
+        
+        EXIT_STRS = ["exit"]
+        for phrase in EXIT_STRS:
+            if phrase in text:
+                speak("Goodbye, I hope I see you soon!")
+                exit()
+
+
+        PRICE_STRS = ["google"]
+        for phrase in PRICE_STRS:
+            if phrase in text:
+                speak("What would you like to search?")
+                srch=get_audio()
+                print("What would you like to search?")
+                url="https://google.com/search?q="+ srch
+                webbrowser.get().open(url)
+                print("Here is what I found for" + srch)
+                speak("Here is what i found for"+ srch)
+
+
+        
